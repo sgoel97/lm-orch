4d765fd40c2e94c320e8d615cdb08644a526eb89
Samarth Goel <sgoel9@berkeley.edu>
2024-03-16 22:05:49 -0500

    remove scratch notebooks

--- lm-orch/utils/logging_utils.py
+++ lm-orch/utils/logging_utils.py
@@ -3,11 +3,15 @@ from pathlib import Path
 from datetime import datetime
 
 
-def write_evaluations(evaluations, model_name, dataset_name, split):
+def write_evaluations(
+    evaluations, model_name, dataset_name, split, prefix="", suffix=""
+):
     current_date = datetime.now().strftime("%m_%d_%Y")
     current_time = datetime.now().strftime("%H_%M_%S")
+    prefix = prefix + "_" if prefix != "" else prefix
+    suffix = "_" + suffix if suffix != "" else suffix
     results_dir = Path(
-        f"../results/{current_date}/{model_name}/{dataset_name}_{split}_{current_time}"
+        f"../results/{current_date}/{model_name}/{prefix}{dataset_name}_{split}_{current_time}{suffix}"
     )
     results_dir.mkdir(parents=True, exist_ok=True)
 
